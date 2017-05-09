8c9619af68d71ed2f1fa7c0446f8ab2e7767d538
yuxinfeng <yuxinfeng@baidu.com>
2017-05-09 22:27:15 +0800

    update: 增加rxjava 配置 启动musicgo项目

---
+++ musicgo/app/src/main/java/music/owner/com/musicgo/MusicMainActivity.java
@@ -0,0 +1,10 @@
+package music.owner.com.musicgo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuxinfeng on 17/4/7.
+ */
+
+public class MusicMainActivity extends AppCompatActivity {
+}
---
+++ musicgo/app/src/main/java/music/owner/com/musicgo/model/MusicModel.java
@@ -0,0 +1,28 @@
+package music.owner.com.musicgo.model;
+
+/**
+ * Created by yuxinfeng on 17/3/25.
+ */
+
+public class MusicModel {
+
+    private String text;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
